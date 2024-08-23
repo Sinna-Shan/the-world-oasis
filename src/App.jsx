65d64fb840748e9ebd4eba0f53f6da3c908dbd60
Noah Shan <luth595@gmail.com>
2024-08-23 15:26:25 +0530

    reuseable styled components
    Changes to be committed:
    	modified:   index.html
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/App.jsx
    	new file:   src/styles/GlobalStyles.js
    	modified:   src/ui/Button.jsx
    	new file:   src/ui/Heading.jsx
    	new file:   src/ui/Input.jsx
    	new file:   src/ui/Row.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
+import GlobalStyles from "./styles/GlobalStyles";
+import Button from "./ui/Button";
+import Heading from "./ui/Heading";
+import Row from "./ui/Row";
+
 function App() {
   return (
-    <div>
-      hello world!
-    </div>
-  )
+    <>
+      <GlobalStyles />
+      <Row type="horizontal">
+        <Heading as="h1">hello world!</Heading>
+        <Heading as="h2">hello world!</Heading>
+        <Heading as="h3">hello world!</Heading>
+        <Button> Check in</Button>
+      </Row>
+      <Button variation="primary" size="large">
+        button primary
+      </Button>
+      <Button variation="secondary" size="medium">
+        button secondary
+      </Button>
+      <Button variation="danger" size="small">
+        button danger
+      </Button>
+      <Row type="vertical">
+        <Heading as="h1">hello world!</Heading>
+        <Heading as="h2">hello world!</Heading>
+        <Heading as="h3">hello world!</Heading>
+        <Button> Check in</Button>
+      </Row>
+    </>
+  );
 }
 
-export default App
+export default App;
