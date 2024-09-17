c4061715caee77d6166dc8be67ab1643c0ed4b36
Noah Shan <luth595@gmail.com>
2024-09-17 20:56:55 +0530

    submitting cabin form and handling errors
    Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   src/App.jsx
    	modified:   src/features/cabins/CabinRow.jsx
    	modified:   src/features/cabins/CabinTable.jsx
    	modified:   src/features/cabins/CreateCabinForm.jsx
    	modified:   src/pages/Cabins.jsx
    	modified:   src/services/apiCabins.js
    	modified:   src/ui/AppLayout.jsx
    	new file:   src/ui/FormRow.jsx
    	modified:   src/utils/helpers.js

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,21 @@ import Account from "./pages/Account";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./ui/AppLayout";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { Toaster } from "react-hot-toast";
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    },
+  },
+});
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -27,11 +38,26 @@ function App() {
             <Route path="settings" element={<Settings />} />
             <Route path="account" element={<Account />} />
           </Route>
-            <Route path="login" element={<Login />} />
-            <Route path="*" element={<PageNotFound />} />
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+      <Toaster position="top-center" gutter={12} containerStyle={{ margin: "8px" }} toastOptions={{
+        success: {
+          duration: 3000,
+        },
+        error: {
+          duration: 5000,
+        },
+        style: {
+          fontSize: "16px",
+          maxWidth: "500px",
+          padding: "16px 24px",
+          backgroundColor:"var(--color-gray-0)",
+          color:"var(--color-gray-700)",
+        }
+      }}/>
+    </QueryClientProvider>
   );
 }
 
--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,10 +1,35 @@
+/* eslint-disable no-unused-vars */
 import supabase from "./supabase";
 
 export async function getCabins() {
-    const { data, error } = await supabase.from("cabins").select("*");
-    if (error) { 
-        console.error(error);
-        throw new Error('Cabins could not be loaded');
+  const { data, error } = await supabase.from("cabins").select("*");
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins could not be loaded");
+  }
+  return data;
+}
+
+export async function createCabin(newCabin) { 
+  
+const { data, error } = await supabase
+  .from("cabins")
+  .insert([newCabin])
+    .select();
+  
+    if (error) {
+      console.error(error);
+      throw new Error("Cabins could not be created.");
     }
     return data;
+
+}
+
+export async function deleteCabin(id) {
+  const { error, data } = await supabase.from("cabins").delete().eq("id", id);
+  if (error) {
+    console.error(error);
+    throw new Error("Cabins could not be loaded");
+  }
+  return data;
 }
