02c1bf5157ccf401671d4d88368f195c1300eb73
Noah Shan <luth595@gmail.com>
2024-09-22 21:28:29 +0530

    duplicate cabins

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -36,6 +36,7 @@ export async function createEditCabin(newCabin, id) {
     console.error(error);
     throw new Error("Cabins could not be created.");
   }
+  if (hasImgPath) return data;
 
   const { error: storageError } = await supabase.storage
     .from("cabin-images")
