cb801ae34350ba7230b3562c6d6cffd715c1d9d6
Noah Shan <luth595@gmail.com>
2024-11-06 00:38:44 +0530

    fetching booking data and display in the table
    Changes to be committed:
    	modified:   src/features/bookings/BookingRow.jsx
    	modified:   src/features/bookings/BookingTable.jsx
    	new file:   src/features/bookings/useBookings.js
    	modified:   src/features/cabins/CabinTable-v3.jsx
    	modified:   src/pages/Bookings.jsx
    	modified:   src/services/apiBookings.js
    	modified:   src/ui/Empty.jsx

--- src/features/cabins/CabinTable-v3.jsx
+++ src/features/cabins/CabinTable-v3.jsx
@@ -1,14 +1,12 @@
 /* eslint-disable no-unused-vars */
-import { useQuery } from "@tanstack/react-query";
 
-import styled from "styled-components";
-import { getCabins } from "../../services/apiCabins";
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
 import { useCabins } from "./useCabins";
 import Table from "../../ui/Table";
 import Menus from "../../ui/Menus";
 import { useSearchParams } from "react-router-dom";
+import Empty from "../../ui/Empty";
 
 function CabinTable() {
   const [searchParams] = useSearchParams();
@@ -39,7 +37,7 @@ function CabinTable() {
   ); 
 
   if (isLoading) return <Spinner />;
-
+  if (!cabins.length) return <Empty resourceName="bookings" />;
   return (
     <Menus>
       <Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
