e1cd0f121e0aae43884e41a35fbf7999bebcc76d
Noah Shan <luth595@gmail.com>
2024-11-06 00:54:18 +0530

    upload test data
    Changes to be committed:
    	modified:   src/features/cabins/CabinTable-v3.jsx
    	modified:   src/ui/SideBar.jsx

--- src/features/cabins/CabinTable-v3.jsx
+++ src/features/cabins/CabinTable-v3.jsx
@@ -21,18 +21,19 @@ function CabinTable() {
     filteredCabins = cabins;
   }
   if (filterValue === "no-discount") {
-    filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
+    filteredCabins = cabins?.filter((cabin) => cabin.discount === 0);
   }
   if (filterValue === "with-discount") {
-    filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
+    filteredCabins = cabins?.filter((cabin) => cabin.discount > 0);
   }
 
   // Sort cabins
   const sortBy = searchParams.get("sortBy") || "name-asc";
+  console.log(filteredCabins);
 
   const [field, direction] = sortBy.split("-");
   const modifier = direction === "asc" ? 1 : -1;
-  const sortedCabins = filteredCabins.sort(
+  const sortedCabins = filteredCabins?.sort(
     (a, b) => (a[field] - b[field]) * modifier
   ); 
 
